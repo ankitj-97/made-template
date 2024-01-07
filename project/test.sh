7677baae4382f2a66987f2fc20c8a1c6af694af5
ankitj-97 <84343460+ankitj-97@users.noreply.github.com>
2024-01-08 05:07:27 +0530

    Update test.sh

--- project/test.sh
+++ project/test.sh
@@ -2,8 +2,8 @@
 
 # execute the pipeline
 echo "Execute the pipeline"
-python ./data/pipeline.py
+python ./project/pipeline.py
 
 # test of pipeline
 echo "Test if pipeline works correctly"
-pytest ./data/test.py
+pytest ./project/test.py
